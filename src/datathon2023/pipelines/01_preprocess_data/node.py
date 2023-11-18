0e7f0a946cc90908343fdf983b1a12a447daea4e
dcastf <dcastf01@gmail.com>
2023-11-18 23:21:37 +0100

    we needed to clean our geodata file

--- src/datathon2023/pipelines/01_preprocess_data/node.py
+++ src/datathon2023/pipelines/01_preprocess_data/node.py
@@ -11,5 +11,12 @@ def concat_files(
 
 
 def geodata(geodata: pd.DataFrame, df: pd.DataFrame):
+
+    #after some preprocessing we have detected some values that are not the same in geodata and in df like
+    # La Oliva is Oliva, La
+    geodata= geodata.replace('Oliva, La', 'La Oliva')
+    #We need the unique values in geodata
+    geodata= geodata.drop_duplicates(subset=['municipio_geo'])
     geodata = geodata.merge(df, left_on='municipio_geo', right_on='municipio', how='inner')
+
     return geodata
