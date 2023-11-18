e2a9e2fba1f5cd5e52c072897b5255aed19616c8
dcastf <dcastf01@gmail.com>
2023-11-18 18:25:34 +0100

    updating

--- src/datathon2023/pipelines/70_ingest_elastic/node.py
+++ src/datathon2023/pipelines/70_ingest_elastic/node.py
@@ -14,22 +14,22 @@ def _gendata(df:pl.DataFrame):
 
 
 
-def upload_data_to_elastic(df,mapping,cloud_id=os.environ['cloud_id'],api_key=os.environ['apikey_key']):
+def upload_data_to_elastic(df,mapping,index,cloud_id=os.environ['cloud_id'],api_key=os.environ['apikey_key']):
 
     # api_key=(apikey_id,apikey_key)
 
     es=Elasticsearch(cloud_id=cloud_id,api_key=api_key)
     #check the index existe and if exist remove it
-    if es.indices.exists(index='datathon2023'):
-        es.indices.delete(index='datathon2023')
-    es.indices.create(index='datathon2023',body={
+    if es.indices.exists(index=index):
+        es.indices.delete(index=index)
+    es.indices.create(index=index,body={
         "settings": {'number_of_shards': 1},
         "mappings":          mapping
 
     })
     number_of_docs=df.shape[0]
     progress=tqdm(total=number_of_docs,desc='Uploading data to elastic')
-    for ok,action in helpers.streaming_bulk(client=es,index='datathon2023',actions=_gendata(df)):
+    for ok,action in helpers.streaming_bulk(client=es,index=index,actions=_gendata(df)):
         if not ok:
             print(action)
         progress.update(1)
